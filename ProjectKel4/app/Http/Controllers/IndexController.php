012e0b53367b90058bdce42f948baafcb627c9cc
StevenTI1191 <Steven23ti@mahasiswa.pcr.ac.id>
2024-11-11 11:30:04 +0700

    update

---
+++ ProjectKel4/app/Http/Controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class PasienController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('index');
+    }
+
+
+}
---
+++ ProjectKel4/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    Nama Anggota Kelompok :
+    1. Andika
+    2. Dhea
+    3. Mifta
+    4. Fitra
+    5. Steven
+</body>
+</html>
