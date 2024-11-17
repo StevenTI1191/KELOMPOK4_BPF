e23b50c37ac64511363d746d3f41d096d742c471
StevenTI1191 <Steven23ti@mahasiswa.pcr.ac.id>
2024-11-17 18:31:57 +0700

    Nama Kelompok

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-class PasienController extends Controller
+class IndexController extends Controller
 {
     /**
      * Display a listing of the resource.
