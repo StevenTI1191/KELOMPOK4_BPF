ea9b2720e4ee13f77c8397a385246fd5bb83d031
andikasyhd <andika23ti@mahasiswa.pcr.ac.id>
2024-11-17 17:25:37 +0700

    coba

---
+++ resources/views/data.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View Baru</title>
+</head>
+<body>
+    <h1>Selamat Datang di Laravel!</h1>
+    <p>Ini adalah kata-kata yang ditampilkan di view baru.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/data', function () {
+    return view('data');
+});
+
+
