25a39e0747f568d215bcc970fd50728466f64dd3
StevenTI1191 <Steven23ti@mahasiswa.pcr.ac.id>
2024-12-02 10:32:13 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,11 @@ route::get('steven',[App\Http\Controllers\IndexController::class,'steven']);
 route::get('fitra', [App\Http\Controllers\IndexController::class,'fitra']);
 route::get('mifta', [App\Http\Controllers\IndexController::class,'mifta']);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
