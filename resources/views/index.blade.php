df30f59bd30ae95a33d388e521e49938785e4d64
StevenTI1191 <Steven23ti@mahasiswa.pcr.ac.id>
2024-12-15 22:09:03 +0700

    Layanan dan Fasilitas Perpustakaan Homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -159,9 +159,7 @@
                   <input type="text" name="name" class="form-control" placeholder="Name" required="">
                 </div>
 
-                <div class="col-12 ">
-                  <input type="email" class="form-control" name="email" placeholder="Email" required="">
-                </div>
+                
 
                 <div class="col-12">
                   <input type="text" class="form-control" name="phone" placeholder="Phone" required="">
@@ -194,8 +192,8 @@
 
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
-        <h2>Constructions</h2>
-        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        <h2>Layanan dan Fasilitas Perpustakaan</h2>
+        <p>Cek Fasilitas Perpustakaan Kami yang Lengkap dan Menunjang Kebutuhan Belajar Anda!</p>
       </div><!-- End Section Title -->
 
       <div class="container">
@@ -206,12 +204,13 @@
             <div class="card-item">
               <div class="row">
                 <div class="col-xl-5">
-                  <div class="card-bg"><img src="assets/img/constructions-1.jpg" alt=""></div>
+                  <div class="card-bg"><img src="assets/img/ruangBaca.jpg" alt=""></div>
                 </div>
                 <div class="col-xl-7 d-flex align-items-center">
                   <div class="card-body">
-                    <h4 class="card-title">Eligendi omnis sunt veritatis.</h4>
-                    <p>Fuga in dolorum et iste et culpa. Commodi possimus nesciunt modi voluptatem placeat deleniti adipisci. Cum delectus doloribus non veritatis. Officia temporibus illo magnam. Dolor eos et.</p>
+                    <h4 class="card-title">BI Corner</h4>
+                    <p>Nikmati kenyamanan ruang baca kami yang tenang dan nyaman, dilengkapi dengan kursi dan meja untuk menunjang kegiatan membaca buku, 
+                      majalah, atau jurnal. Tempat yang ideal untuk fokus dan menyusun bahan bacaan dengan nyaman.</p>
                   </div>
                 </div>
               </div>
@@ -222,12 +221,13 @@
             <div class="card-item">
               <div class="row">
                 <div class="col-xl-5">
-                  <div class="card-bg"><img src="assets/img/constructions-2.jpg" alt=""></div>
+                  <div class="card-bg"><img src="assets/img/KoleksiBuku.jpg" alt=""></div>
                 </div>
                 <div class="col-xl-7 d-flex align-items-center">
                   <div class="card-body">
-                    <h4 class="card-title">Possimus ut sed velit assumenda</h4>
-                    <p>Sunt deserunt maiores voluptatem autem est rerum perferendis rerum blanditiis. Est laboriosam qui iste numquam laboriosam voluptatem architecto. Est laudantium sunt at quas aut hic. Eum dignissimos.</p>
+                    <h4 class="card-title">Koleksi Buku Dan Referensi</h4>
+                    <p>Temukan berbagai koleksi buku dari berbagai bidang ilmu, baik fiksi maupun non-fiksi, serta sumber referensi seperti ensiklopedia, 
+                      kamus, dan atlas. Semua bahan yang Anda butuhkan untuk penelitian tersedia di sini.</p>
                   </div>
                 </div>
               </div>
@@ -238,12 +238,13 @@
             <div class="card-item">
               <div class="row">
                 <div class="col-xl-5">
-                  <div class="card-bg"><img src="assets/img/constructions-3.jpg" alt=""></div>
+                  <div class="card-bg"><img src="assets/img/KerjaKelompok.jpg" alt=""></div>
                 </div>
                 <div class="col-xl-7 d-flex align-items-center">
                   <div class="card-body">
-                    <h4 class="card-title">Error beatae dolor inventore aut</h4>
-                    <p>Dicta porro nobis. Velit cum in. Nesciunt dignissimos enim molestiae facilis numquam quae quaerat ipsam omnis. Neque debitis ipsum at architecto officia laboriosam odit. Ut sunt temporibus nulla culpa.</p>
+                    <h4 class="card-title">Ruang Diskusi Dan Kelompok Belajar</h4>
+                    <p>Gunakan ruang diskusi kami untuk bekerja sama dalam proyek atau studi kelompok. Dilengkapi dengan meja besar, 
+                      papan tulis, dan peralatan lainnya, ruangan ini sangat ideal untuk kolaborasi dan pembelajaran bersama.</p>
                   </div>
                 </div>
               </div>
@@ -254,12 +255,13 @@
             <div class="card-item">
               <div class="row">
                 <div class="col-xl-5">
-                  <div class="card-bg"><img src="assets/img/constructions-4.jpg" alt=""></div>
+                  <div class="card-bg"><img src="assets/img/AksesInternet.jpg" alt=""></div>
                 </div>
                 <div class="col-xl-7 d-flex align-items-center">
                   <div class="card-body">
-                    <h4 class="card-title">Expedita voluptas ut ut nesciunt</h4>
-                    <p>Aut est quidem doloremque voluptatem magnam quis excepturi vero quia. Eum eos doloremque architecto illo at beatae dolore. Fugiat suscipit et sint ratione dolores. Aut aliquid ea dolores libero nobis.</p>
+                    <h4 class="card-title">Komputer dan Akses Internet</h4>
+                    <p>Gunakan fasilitas komputer dengan akses internet untuk mencari informasi, menjelajahi database digital, dan melakukan penelitian. 
+                      Beberapa perpustakaan juga menyediakan software khusus untuk mendukung kegiatan belajar dan penelitian Anda.</p>
                   </div>
                 </div>
               </div>
